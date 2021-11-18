de624e29fe79be9f53395773312df9a58e77570b
Roely Chur <gerson.roely@gmail.com>
2021-11-18 10:07:20 -0600

    Total Anual Individual

--- src/components/dashboard/Individual.vue
+++ src/components/dashboard/Individual.vue
@@ -92,7 +92,15 @@
                     <v-col cols="12">
                         <h5>
                             <strong>
-                                PUNTAJE ANUAL: {{ colaborador.total_mensual.toFixed(2) }} PUNTOS
+                                PUNTAJE ANUAL: 
+                                <v-tooltip right>
+                                    <template v-slot:activator="{on, attrs}">
+                                        <span v-bind="attrs" v-on="on">{{ colaborador.total_anual.toFixed(2) }}</span>
+                                    </template>
+                                    <div v-for="(date, key) in colaborador.dates" :key="key">
+                                        <span>{{ date }}</span>
+                                    </div>
+                                </v-tooltip>
                             </strong>
                         </h5>
                     </v-col>
@@ -150,6 +158,19 @@
                     .then((response) => {
                         this.colaborador = response.data
                         this.isLoading = false
+
+                        const data_ = {
+                            url: 'puntaje_anual',
+                            data: {
+                                empleado: this.colaborador
+                            }
+                        }
+
+                        request.post(data_)
+                        .then((response) => {
+                            console.log(response.data)
+                            this.colaborador = response.data
+                        })
                     })
 
                 })
